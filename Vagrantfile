7910f5f4a8d6728fc2be0699512e8cafdc3ca27c
Jason Kulatunga <jason@thesparktree.com>
2018-12-12 21:09:01 -0800

    adding commands for building with yum.

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,5 @@
 Vagrant.configure("2") do |config|
-    config.vm.box = "debian/jessie64"
+    config.vm.box = "centos/7"
 
     config.vm.provider "virtualbox" do |v|
         v.name = "coreos_builder"
--- provisioner.sh
+++ provisioner.sh
@@ -1,35 +1,40 @@
-FROM debian:jessie-slim
+#!/usr/bin/env bash
 
 ## Prerequisites
 
 # meteor installer doesn't work with the default tar binary
-apt-get update && \
-    apt-get install -y --no-install-recommends \
+yum install -y \
     ca-certificates \
     curl \
     git \
-    sudo \
-    bzip2 && \
-    rm -rf /var/lib/apt/lists/*
+    bzip2
 
 cd /usr/bin && \
-    curl -L -o cork https://github.com/coreos/mantle/releases/download/v0.11.1/cork-0.11.1-amd64 && \
+    curl -L -o cork https://github.com/coreos/mantle/releases/download/v0.10.0/cork-0.10.0-amd64 && \
     chmod +x cork && \
-    which cork && \
-    git config --global user.email "jason@thesparktree.com" && \
-    git config --global user.name "Jason Kulatunga"
+    which cork
 
 ## Using Cork
-# https://coreos.com/os/docs/latest/sdk-modifying-coreos.html
+# https://coreos.com/os/docs/latest/sdk-modifying-coreos.html=
 
-mkdir -p /coreos-sdk
+exec sudo -u vagrant /bin/sh - << 'EOF'
+whoami
+git config --global user.email "jason@thesparktree.com" && \
+git config --global user.name "Jason Kulatunga"
 
-cd /coreos-sdk && \
-    mkdir -p /home/root/ && \
-    /usr/bin/cork create
+mkdir -p ~/coreos-sdk
+cd ~/coreos-sdk
+cork create --sdk-version 1911.4.0 --replace
 
-/usr/bin/cork enter && \
-    grep NAME /etc/os-release # Verify you are in the SDK chroot
+cork enter
+grep NAME /etc/os-release
 
-## Building an image
+./set_shared_user_password.sh 12345
+./setup_board
+./build_packages
+./build_image
+
+
+EOF
+
 
